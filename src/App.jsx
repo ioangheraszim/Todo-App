3c374e09ce699eee38dbf237b2c518f6a76d3c1d
ioangheraszim <gijohny17@gmail.com>
2023-07-26 23:22:48 +0100

    managed to fix the logo and theme changer button

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ function App() {
   return (    
     <div class="container">
       <div class="top-background"></div>      
-      <div class="todo-list">
+      <div class="wrapper">
         <div class="header">
-            <div class="logo">Todo</div>
-            <button class="change-button">Change</button>
+            <div class="logo">
+              <h1>TODO</h1>
+            </div>
+            <div className="theme-btn">
+              <button class="change-button"><img src='./src/assets/images/icon-sun.svg'/></button>
+            </div>           
         </div>
         <div className="list-items">
           <input type="text" class="input-field" placeholder="Add a new item..." />
