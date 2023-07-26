046791202b647a5f45990e34ee19baa0b37628f3
ioangheraszim <gijohny17@gmail.com>
2023-07-26 23:03:49 +0100

    having difficulty in styling the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
-
 function App() {  
-  return (
-    <>
-      <div>
-        <h1>Setting up files</h1>
-        <p>and other stuff</p>
+  return (    
+    <div class="container">
+      <div class="top-background"></div>      
+      <div class="todo-list">
+        <div class="header">
+            <div class="logo">Todo</div>
+            <button class="change-button">Change</button>
+        </div>
+        <div className="list-items">
+          <input type="text" class="input-field" placeholder="Add a new item..." />
+          <div class="todo-item">
+            <input type="checkbox" class="checkbox" />
+            <span>Item 1</span>
+          </div> 
+        </div>
+          
       </div>
-    </>
+    </div>    
   )
 }
 
