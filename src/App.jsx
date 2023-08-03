92b8b8e0a22b89db1517ea7d47aacd39e3912230
ioangheraszim <gijohny17@gmail.com>
2023-08-03 22:06:16 +0100

    i have deleted the bottom part by mistake had to re do it, 4  more functionalities left to do

--- src/App.jsx
+++ src/App.jsx
@@ -148,6 +148,24 @@ function App() {
               </div>
             );
           })}
+          <div className="footer">
+            <div className="left-side">
+              <p>{todos.length} items left</p>
+            </div>
+            <div className="button-wrapper">
+              <button>All</button>
+              <button>Active</button>
+              <button>Completed</button>
+            </div>
+            <div className="clear-button">
+              <button className="clear-btn">Clear Completed</button>
+            </div>
+          </div>
+          <div className="button-wrapper-mb">
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+          </div>
         </div>
         <BottomText task={todos} />
       </div>
